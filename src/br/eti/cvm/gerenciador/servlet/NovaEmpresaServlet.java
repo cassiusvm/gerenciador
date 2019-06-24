4584b19142b767ccf20ff19d3c81d706a51e4a03
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2019-06-23 22:38:46 -0300

    Apenas POST

--- src/br/eti/cvm/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/eti/cvm/gerenciador/servlet/NovaEmpresaServlet.java
@@ -14,7 +14,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nomeEmpresa = request.getParameter("nome");
 		
 		PrintWriter saidaHtml = response.getWriter();
