5ddca6a514419ae21afcdeabaeff95c333e627e2
cassius <cassius@cassius-ubuntuVM>
2019-06-26 17:18:17 -0300

    Escolhendo o tipo de saida json ou xml

--- src/br/eti/cvm/gerenciador/servlet/EmpresasService.java
+++ src/br/eti/cvm/gerenciador/servlet/EmpresasService.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
 
 import br.eti.cvm.gerenciador.modelo.BancoDados;
 import br.eti.cvm.gerenciador.modelo.Empresa;
@@ -21,13 +22,29 @@ public class EmpresasService extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String paramSaida = request.getParameter("saida");
+		
 		List<Empresa> empresas = new BancoDados().getEmpresas();
 		
-		Gson gson = new Gson();
-		String json = gson.toJson(empresas);
+		String saida = null;
+		String contentType = null;
 		
-		response.setContentType("application/json");
-		response.getWriter().print(json);
+		if(paramSaida.equals("json")) {
+			Gson gson = new Gson();
+			saida = gson.toJson(empresas);
+			contentType="application/json";
+			
+		} else if(paramSaida.equals("xml")) {
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+			saida = xstream.toXML(empresas);
+			contentType="application/xml";
+		}
+
+		if(paramSaida.equals("json") || paramSaida.equals("xml")) {
+			response.setContentType(contentType);
+			response.getWriter().print(saida);
+		}
 	}
 
 }
