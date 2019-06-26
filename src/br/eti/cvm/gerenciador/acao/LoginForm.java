e9ee8883c06bdfc7e10a0df81948588a3f4a2140
cassius <cassius@cassius-ubuntuVM>
2019-06-26 14:46:12 -0300

    Primeiro Filtro

--- src/br/eti/cvm/gerenciador/acao/LoginForm.java
+++ src/br/eti/cvm/gerenciador/acao/LoginForm.java
@@ -6,8 +6,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.eti.cvm.gerenciador.modelo.BancoDados;
-
 public class LoginForm  implements AcaoExecutavel {
 
 	@Override
